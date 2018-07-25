372cc3eda6b17703d105b621c8b6d5185d698a7a
beto <albertocisc@openmailbox.org>
2018-07-25 13:33:45 -0500

    Adds CardEntity to handle the current card to GameScene

--- include/GameScene.hpp
+++ include/GameScene.hpp
@@ -2,6 +2,7 @@
 #define GAME_SCENE_HPP
 
 #include "Deck.hpp"
+#include "CardEntity.hpp"
 
 #include <LK/Scene.hpp>
 using namespace lk;
@@ -12,8 +13,12 @@ public:
 	void onCreate() override;
 	void onClose(int scene) override;
 
+	bool setCurrentCard(Card *card);
+	const Card * getCurrentCard() const;
+
 private:
 	Deck deck;
+	CardEntity *currentCardEntity;
 };
 
 #endif // GAME_SCENE_HPP
